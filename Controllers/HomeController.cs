32f5f1305e0cb1427b2dd09dd046f1709b1b3036
Daniel Polo <daniel_polo@live.com>
2020-07-31 20:47:54 -0300

    Criado o Layout Basse e tela de login

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using PoloTech_Estoque.Models;
 
 namespace PoloTech_Estoque.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
