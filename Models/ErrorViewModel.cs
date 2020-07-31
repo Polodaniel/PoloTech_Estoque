f61cd710c6dd21550f2926c368d5e5823b6c65a5
Daniel Polo <daniel_polo@live.com>
2020-07-31 18:47:56 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PoloTech_Estoque.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
